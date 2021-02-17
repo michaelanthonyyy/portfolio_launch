8b186b4ea9b902b6fbaa4e622b4fd3f50a3460e9
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-16 19:16:37 -0800

    added javascript to modal buttons to open and close

---
+++ public/js/projects.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $("#launchModalOne").click(function () {
+
+        $(".modal").addClass("is-active");
+
+    });
+
+    $("#launchModalTwo").click(function () {
+
+        $(".modal").addClass("is-active");
+
+    });
+
+    $("#launchModalThree").click(function () {
+
+        $(".modal").addClass("is-active");
+
+    });
+
+    $(".delete").click(function () {
+
+        $(".modal").removeClass("is-active");
+
+    });
+
+});
